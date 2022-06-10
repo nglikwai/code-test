3b6df0bf6b7b72850d0e5c37dfd50a2d39dcdeb0
LIK WAI NG <nglikwai@ymail.com>
2022-06-11 00:39:41 +0800

    fix: uncontrolled input and ref

--- src/Switch/index.tsx
+++ src/Switch/index.tsx
@@ -12,33 +12,38 @@ interface State {
 }
 
 export default class Switch extends React.PureComponent<Props, State> {
+
+    inputRef: React.RefObject<HTMLInputElement>;
+
     constructor(props: Props) {
         super(props);
+        this.inputRef = React.createRef();
         this.state = {
-          _checked: false
+            _checked: false
         }
     }
 
     OnClickHandler = () => {
-      const { checked, onChange } = this.props;
-      onChange
-          ? onChange(!checked)
-          : this.setState(({ _checked }) => { _checked: !_checked })
+        const { checked, onChange } = this.props;
+        onChange
+            ? onChange(!checked)
+            : this.setState(({ _checked }) => { _checked: !_checked })
     }
 
     render() {
-      const { disabled } = this.props
-      return (
-          <div className="comp-switch">
-              <label className="switch">
-                  <input
-                      type="checkbox"
-                      disabled={disabled}
-                      onClick={this.OnClickHandler}
-                  />
-                  <div className="slider"></div>
-              </label>
-          </div>
-      );
+        const { disabled } = this.props
+        return (
+            <div className="comp-switch">
+                <label className="switch">
+                    <input
+                        ref={this.inputRef}
+                        type="checkbox"
+                        disabled={disabled}
+                        onClick={this.OnClickHandler}
+                    />
+                    <div className="slider"></div>
+                </label>
+            </div>
+        );
     }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Switch from "./Switch"
+import "./index.css";
 
-interface Props {}
+
+interface Props { }
 
 interface State {
     isSwitchChecked: boolean;
     isSwitchDisabled: boolean;
+    uncontrolledChecked: boolean | null;
 }
 
 class App extends React.PureComponent<Props, State> {
@@ -14,31 +17,51 @@ class App extends React.PureComponent<Props, State> {
     public state: State = {
         isSwitchChecked: false,
         isSwitchDisabled: false,
+        uncontrolledChecked: false
     };
+
+    ref = React.createRef<Switch>()
 
     onToggleDisable = () => this.setState(({ isSwitchDisabled }) => ({ isSwitchDisabled: !isSwitchDisabled }));
 
     onChange = (checked: boolean) => this.setState({ isSwitchChecked: checked });
 
+    getUncontrolledChecked = () => this.setState({
+        uncontrolledChecked: this.ref.current
+            && this.ref.current.inputRef.current
+            && this.ref.current.inputRef.current.checked
+    })
+
     render() {
-      const { isSwitchChecked, isSwitchDisabled } = this.state
+        const { isSwitchChecked, isSwitchDisabled, uncontrolledChecked } = this.state
+        console.log()
         return (
-            <div>
-                <h3>Controlled Switch</h3>
-                <Switch
-                    disabled={isSwitchDisabled}
-                    checked={isSwitchChecked}
-                    onChange={this.onChange}
-                />
+            <div className="wrapper">
+                <div>
+                    <h3>Controlled Switch</h3>
+                    <Switch
+                        disabled={isSwitchDisabled}
+                        checked={isSwitchChecked}
+                        onChange={this.onChange}
+                    />
+                </div>
+                <div>
+                    <h3>Uncontrolled Switch</h3>
+                    <h4>uncontrolled check : {JSON.stringify(uncontrolledChecked)}
+                    </h4>
 
-                <h3>Uncontrolled Switch</h3>
-                <Switch disabled={isSwitchDisabled} />
+                    <Switch ref={this.ref} disabled={isSwitchDisabled} />
+                    <button onClick={this.getUncontrolledChecked}>uncontrolled Status</button>
 
-                <h3>isSwitchDisabled : {JSON.stringify(isSwitchDisabled)}</h3>
 
-                <button type="button" onClick={this.onToggleDisable}>
-                    Toggle Disable
-                </button>
+                </div>
+                <div>
+                    <h3>isSwitchDisabled : {JSON.stringify(isSwitchDisabled)}</h3>
+                    <button type="button" onClick={this.onToggleDisable}>
+                        Toggle Disable
+                    </button>
+                </div>
+
             </div>
         );
     }
